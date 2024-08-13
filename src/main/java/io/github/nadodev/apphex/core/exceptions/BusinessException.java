f677a8ee7fa05a9e0d14e8c6db765321cd0a4f42
leoanrdo <programacao@studioalpha.com.br>
2024-08-13 08:34:24 -0300

    feat: add exception

---
+++ src/main/java/io/github/nadodev/apphex/core/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package io.github.nadodev.apphex.core.exceptions;
+
+public class BusinessException extends  RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
