d0ad0050ce31d41f33d2241d0839b8cef760ae0f
leoanrdo <programacao@studioalpha.com.br>
2024-08-13 08:33:33 -0300

    initial commit

---
+++ src/main/java/io/github/nadodev/apphex/AppHexApplication.java
@@ -0,0 +1,13 @@
+package io.github.nadodev.apphex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppHexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/nadodev/apphex/AppHexApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nadodev.apphex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
